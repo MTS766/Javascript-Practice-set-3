030e76ac9378e8e408699e16b177033fc1cd1437
MTS766 <103117672+MTS766@users.noreply.github.com>
2023-09-04 19:24:41 +0500

    practice-set-3

---
+++ Index.js
@@ -0,0 +1,99 @@
+// QUSTION NO-1 ------>
+
+// const fruits = ["apple" , "Banana" , "oranges"];
+// console.log(fruits)
+// fruits.push("Grape");
+// console.log(fruits)
+
+
+
+// QUSTION NO-2 ------>
+
+// const number = [1 , 2 , 3 , 4 , 5];
+// console.log(number);
+// number.pop();
+// console.log(number);
+
+
+
+// QUSTION NO-3 ------>
+
+// const  colors = ["Red", "Green", "Blue"];
+// console.log(colors);
+// colors.unshift("Yellow");
+// console.log(colors);
+
+
+
+// QUSTION NO-4 ------>
+
+// const  days = ["Monday", "Tuesday", "Wednesday", "Thursday"];
+// console.log(days);
+// days.shift();
+// console.log(days);
+
+
+
+// QUSTION NO-5 ------>
+
+// const names = [];
+// console.log(names)
+// names.push("Alice" , "Bob" , "Charlie");
+// console.log(names)
+
+
+
+// QUSTION NO-6 ------>
+
+// let  numbers = [10, 20, 30, 40, 50];
+// let sum = 0;
+// for(let i=0 ; i<numbers.length ; i++)
+// {
+//     sum = sum+numbers[i];
+//     console.log(sum);
+// }
+
+
+
+// QUSTION NO-7 ------>
+
+// let evenNumbers = []
+// for(let i=2 ; i <= 20 ; i=i+2)
+// {
+//     evenNumbers.push(i);
+// }
+// console.log(evenNumbers);
+
+
+
+// QUSTION NO-8 ------>
+
+// let oddNumbers = []
+// for(let i=1 ; i <= 15 ; i=i+2)
+// {
+//     oddNumbers.push(i);
+// }
+// console.log(oddNumbers);
+
+
+
+// QUSTION NO-9 ------>
+
+// let  fruits = ["apple", "banana", "orange", "kiwi"];
+// for(let i=0 ; i<fruits.length ; i++)
+// {
+//     console.log(fruits[i]);
+// }
+
+
+
+// QUSTION NO-10 ------>
+
+// let  animals = ["cat", "dog", "elephant", "giraffe"];
+// for(let i=0 ; i<animals.length ; i++)
+// {
+//     if(animals[i]==="elephant")
+//     {
+//         console.log(i);
+//     }
+// }
